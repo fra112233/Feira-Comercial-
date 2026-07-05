24cf178e4cbec9c5ed6a7ceb6748f1f507e0d9fd
FRA <francisconarcisomachaeie@gmail.com>
2026-07-05 15:29:31 +0400

    Add files via upload

---
+++ supabase-config.j/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://b1jnkwrhqnpawclybqrw.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_qvUvtBjpAKtMevafHRJe8g_W-aUkOb0';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
